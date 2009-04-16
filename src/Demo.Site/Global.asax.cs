9101b6204fee453fd037c3e547de35d13f1bcd51
Peter Mounce <pete@neverrunwithscissors.com>
2009-04-17 00:01:52 +0100

    Wire up quick & dirty ioc

--- src/Demo.Site/Global.asax.cs
+++ src/Demo.Site/Global.asax.cs
@@ -1,32 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+
+using Microsoft.Practices.ServiceLocation;
+
+using Nrws.IncludeCombiner;
 
 namespace Demo.Site
 {
 	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
 	// visit http://go.microsoft.com/?LinkId=9394801
 
-	public class MvcApplication : System.Web.HttpApplication
+	public class MvcApplication : HttpApplication
 	{
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapRoute(
-				"Default",                                              // Route name
-				"{controller}/{action}/{id}",                           // URL with parameters
-				new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
-			);
-
+				"Default", // Route name
+				"{controller}/{action}/{id}", // URL with parameters
+				new { controller = "Home", action = "Index", id = "" } // Parameter defaults
+				);
 		}
 
 		protected void Application_Start()
 		{
 			RegisterRoutes(RouteTable.Routes);
+			ServiceLocator.SetLocatorProvider(() => new QnDServiceLocator());
 		}
 	}
 }

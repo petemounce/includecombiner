6342acdddcdca7cff225e2141cc0a2f8e7ad7063
Peter Mounce <pete@neverrunwithscissors.com>
2009-04-18 00:31:37 +0100

    Match name to class under test

---
+++ src/Nrws.Integration.Tests/Web/IncludeHandling/IncludeReaderFacts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Nrws.Web.IncludeHandling;
+
+using Xunit;
+using Xunit.Extensions;
+
+namespace Nrws.Integration.Tests.Web.IncludeHandling
+{
+	public class IncludeReaderFacts
+	{
+		private readonly IIncludeReader _reader;
+
+		public IncludeReaderFacts()
+		{
+			_reader = new IncludeReader();
+		}
+
+		[Fact]
+		public void WhenFileExists_WillReadIt()
+		{
+			Include include = null;
+			Assert.DoesNotThrow(() => include = _reader.Read("tests\\exists.txt", IncludeType.Script));
+			Assert.Equal("hello world", include.Content);
+		}
+
+		[Theory]
+		[InlineData("")]
+		[InlineData(null)]
+		public void WhenSourceMissing_WillThrow(string source)
+		{
+			Assert.Throws<ArgumentException>(() => _reader.Read(source, IncludeType.Css));
+		}
+
+		[Fact]
+		public void WhenFileNotFound_WillThrow()
+		{
+			Assert.Throws<InvalidOperationException>(() => _reader.Read("c:\\doesNotExist.txt", IncludeType.Css));
+		}
+	}
+}
